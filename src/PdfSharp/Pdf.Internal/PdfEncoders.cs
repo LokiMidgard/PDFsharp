3571311b25076b1adbb8a5b47912588035cdf30a
Patrick Kranz <patrick-kranz@live.de>
2018-07-08 13:36:14 +0200

    Fix crash in netstandard2.0

--- src/PdfSharp/Pdf.Internal/PdfEncoders.cs
+++ src/PdfSharp/Pdf.Internal/PdfEncoders.cs
@@ -68,7 +68,7 @@ namespace PdfSharp.Pdf.Internal
             {
                 if (_winAnsiEncoding == null)
                 {
-#if !SILVERLIGHT && !NETFX_CORE && !UWP
+#if !SILVERLIGHT && !NETFX_CORE && !UWP && !NETSTANDARD2_0
                     // Use .net encoder if available.
                     _winAnsiEncoding = Encoding.GetEncoding(1252);
 #else
